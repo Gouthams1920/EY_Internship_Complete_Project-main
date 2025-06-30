0953d7ecd3bf3608e7c976201f485a41a5447da9
Gouthams1920 <gouthams1920@gmail.com>
2025-06-30 13:50:43 +0530

    w

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/Sidebar/Sidebar'
-import { Routes,Route } from 'react-router-dom'
-import Add from './pages/Add/Add'
-import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
+import React, { createContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Orders from './pages/Orders/Orders';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const App = () => {
+// Create Context for URL
+export const AppContext = createContext();
 
-  const url = "http://localhost:4000"
+const App = () => {
+  const url = "http://localhost:4000";
 
   return (
-    <div>
+    <AppContext.Provider value={{ url }}>
       <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path="/add" element={<Add url={url}/>} />
-          <Route path="/list" element={<List url={url}/>} />
-          <Route path="/orders" element={<Orders url={url}/>} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
         </Routes>
       </div>
-    </div>
-  )
-}
+    </AppContext.Provider>
+  );
+};
 
-export default App
+export default App;
